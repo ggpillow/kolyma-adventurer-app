226c25082b812da27551a77b2c047c537a391d80
ggpillow <ketnut3336@gmail.com>
2025-05-22 19:55:12 +1100

    fix1

---
+++ server/src/main/java/com/kolyma/adventure/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.kolyma.adventure.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/healthz")
+    public String health() {
+        return "OK";
+    }
+}
