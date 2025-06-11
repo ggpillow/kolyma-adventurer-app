9474f811634cb2672825c8448a0cefc29d38fe1f
ggpillow <ketnut3336@gmail.com>
2025-06-12 00:07:00 +1100

    new data of items

--- server/src/main/java/com/kolyma/adventure/repository/ItemCombinationRepository.java
+++ server/src/main/java/com/kolyma/adventure/repository/ItemCombinationRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 public interface ItemCombinationRepository extends JpaRepository<ItemCombination, Long> {
 
     @Query("SELECT ic FROM ItemCombination ic " +
-            "WHERE (ic.resource1.id = :res1 AND ic.resource2.id = :res2) " +
-            "   OR (ic.resource1.id = :res2 AND ic.resource2.id = :res1)")
+            "WHERE (ic.firstResource.id = :res1 AND ic.secondResource.id = :res2) " +
+            "   OR (ic.firstResource.id = :res2 AND ic.secondResource.id = :res1)")
     Optional<ItemCombination> findByResourcePair(@Param("res1") Long res1, @Param("res2") Long res2);
 }
